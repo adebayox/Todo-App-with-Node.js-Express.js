a93df508238500bec078590758cd05c90346e425
mac <dadebayo200@gmail.com>
2022-09-25 15:22:56 +0100

    todo app ready

---
+++ routes/todo.js
@@ -0,0 +1,32 @@
+const router = require('express').Router()
+const Todo = require("../models/Todo")
+
+
+
+// routes
+
+router.post("/add/todo", (req, res) => {
+    const { todo } = req.body;
+    const newTodo = new Todo({ todo });
+
+    // save todo
+    newTodo.save()
+    .then(() => {
+        console.log("successfuly added todo!")
+        res.redirect("/")
+    })
+    .catch((err) => console.log(err));
+})
+
+
+.get("/delete/todo/:_id",(req, res) => {
+    const {_id} = req.params;
+    Todo.deleteOne({_id})
+    .then(() => {
+        console.log("Deleted Todo Successfully");
+        res.redirect("/");
+    })
+    .catch((err) => console.log(err));
+} )
+
+module.exports = router;
